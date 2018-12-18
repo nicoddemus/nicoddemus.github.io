7ab74b78e460ec8813b588d6d164d0399705276e
Bruno Oliveira <nicoddemus@gmail.com>
2018-12-18 18:33:10 -0200

    Update URL to use HTTPS

--- _src/publishconf.py
+++ _src/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://nicoddemus.github.io'
+SITEURL = 'https://nicoddemus.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'atom.xml'
